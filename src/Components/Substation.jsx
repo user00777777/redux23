4f4b1ab1f7f1b8dcb6675fb4c042f6088d8d98d7
user00777777 <misterborman@gmail.com>
2023-05-09 00:26:06 +0300

    attempt

--- src/Components/Substation.jsx
+++ src/Components/Substation.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
-
+import s from '../css/Substation.module.css'
 export default function Substation() {
   return (
-    <div>Substation</div>
+    <div>
+<div className={[s.parent].join(' ')}>
+<div className={[s.div1,s.vmt1,s.r].join(' ')}> </div>
+<div className={[s.div2,s.t1,s.r].join(' ')}> </div>
+<div className={[s.div3,s.connection,s.r].join(' ')}> </div>
+<div className={[s.div4,s.elCount,s.r].join(' ')}> df</div>
+<div className={[s.div5,s.elCell,s.r].join(' ')}> dfvdffv</div>
+<div className={[s.div6,s.elCell,s.r].join(' ')}> df</div>
+<div className={[s.div7,s.avm1,s.r].join(' ')}>fv </div>
+<div className={[s.div8,s.elCell,s.r].join(' ')}> df</div>
+<div className={[s.div9,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div10,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div11,s.vmt2,s.r].join(' ')}> </div>
+<div className={[s.div12,s.t2,s.r].join(' ')}> </div>
+<div className={[s.div13,s.connection,s.r].join(' ')}> </div>
+<div className={[s.div14,s.elCount,s.r].join(' ')}> </div>
+<div className={[s.div15,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div16,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div17,s.avm2,s.r].join(' ')}> </div>
+<div className={[s.div18,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div19,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div20,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div21,s.elSec,s.r].join(' ')}>dc </div>
+<div className={[s.div22,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div23,s.elCell,s.r].join(' ')}> </div>
+<div className={[s.div24,s.elCell,s.r].join(' ')}> hhh</div>
+<div className={[s.div25,s.emptyCell,s.r].join(' ')}> </div>
+<div className={[s.div26,s.thinks,s.r].join(' ')}> </div>
+<div className={[s.div27,s.fireShield,s.r].join(' ')}> </div>
+<div className={[s.div28,s.door,s.r].join(' ')}> </div>
+<div className={[s.div29,s.door,s.r].join(' ')}> </div>
+<div className={[s.div30,s.door,s.r].join(' ')}> </div>
+</div>
+
+
+
+
+    </div>
   )
 }
