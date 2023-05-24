d98c456219d981d1e02ffc1168086a77e37b5e36
user00777777 <misterborman@gmail.com>
2023-05-24 22:41:35 +0300

    dd

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -19,16 +19,16 @@ export  default function Homepage() {
 <div className={[s.div5,s.elCell,s.r,s.connection].join(' ')}>вв </div>
 <div className={[s.div6,s.elCell,s.r,'o',s.connection].join(' ')}   title={'k'} > вв</div>
 <div className={[s.div7,s.avm1,s.r,].join(' ')  }ref={refA} data-cell='avm1' > Авм <span>#1</span></div>
-<div className={[s.div8,s.elCell,s.r].join(' ')}> яч#6</div>
+<div className={[s.div8,s.elCell,s.r,s.avm2].join(' ')}> Авм#2</div>
 <div className={[s.div9,s.elCell,s.r].join(' ')}> №13</div>
 <div className={[s.div10,s.elCell,s.r].join(' ')}>№14 </div>
 <div className={[s.div11,s.vmt2,s.r].join(' ')}>№2  </div>
 <div className={[s.div12,s.t2,s.r].join(' ')}> <h5> №3</h5></div>
-<div className={[s.div13,s.connection,s.r,s.con2].join(' ')}>Счетчик </div>
-<div className={[s.div14,s.elCount,s.r].join(' ')}> <h5>Счетчик</h5></div>
+<div className={[s.div13,s.r,s.con].join(' ')}>Счетчик </div>
+<div className={[s.div14,s.elCount,s.r,s.con].join(' ')}> <h5>Счетчик</h5></div>
 <div className={[s.div15,s.elCell,s.r].join(' ')}>яч #13 </div>
 <div className={[s.div16,s.elCell,s.r].join(' ')}> №5</div>
-<div className={[s.div17,s.avm2,s.r].join(' ') }ref={refA2} data-cell='avm2'> №6 </div>
+<div className={[s.div17,s.r].join(' ') }ref={refA2} data-cell='avm2'> №6 </div>
 <div className={[s.div18,s.elCell,s.r].join(' ')}>№12 </div>
 <div className={[s.div19,s.elCell,s.r].join(' ')}> №11</div>
 <div className={[s.div20,s.elCell,s.r].join(' ')}> №12</div>
@@ -37,7 +37,7 @@ export  default function Homepage() {
 <div className={[s.div23,s.elCell,s.r].join(' ')}>№9 </div>
 <div className={[s.div24,s.elCell,s.r].join(' ')}>№8  </div>
 <div className={[s.div25,s.emptyCell,s.r].join(' ')}>яч-- </div>
-<div className={[s.div26,s.thinks,s.r].join(' ')}> безопасность</div>
+<div className={[s.div26,s.thinks,s.r].join(' ')}> безпека</div>
 <div className={[s.div27,s.fireShield,s.r].join(' ')}>дверь </div>
 <div className={[s.div28,s.door,s.r].join(' ')}> дверь </div>
 <div className={[s.div29,s.door,s.r].join(' ')}> песок </div>
