ac01346e177714a8ab9820ac89549bb8b4da75ea
user00777777 <misterborman@gmail.com>
2023-08-19 00:35:19 +0300

    dd

--- src/Components/District23/Gate/Gate.jsx
+++ src/Components/District23/Gate/Gate.jsx
@@ -6,21 +6,24 @@ export default function Gate() {
   return (
     <div className={s.mainGates}>
 <div className={s.wrap_left}>
-    <div className={[s.leftgate1,s.gateItem].join(' ')}>1</div>
-    <div className={[s.leftgate2,s.gateItem].join(' ')}>2</div>
-    <div className={[s.leftgate3,s.gateItem].join(' ')}>3</div>
-    <div className={[s.leftgate4,s.gateItem].join(' ')}>4</div>
-    <div className={[s.leftgate5,s.gateItem].join(' ')}>5</div>
+    <div className={[s.leftgate1,s.gateItem].join(' ')}><span>14</span></div>
+    <div className={[s.leftgate2,s.gateItem].join(' ')}><span>10</span></div>
+    <div className={[s.leftgate3,s.gateItem].join(' ')}><span>11</span></div>
+    <div className={[s.leftgate4,s.gateItem].join(' ')}><span>12</span></div>
+    <div className={[s.leftgate5,s.gateItem].join(' ')}><span>13</span></div>
     <div className={[s.leftgate6,s.gateItem].join(' ')}></div>
   
 </div>
 <div className={s.insideContainer}>
 <div className={s.middle}>Вьезд с улицы</div>
 <div className={s.middle}>Телега эстакада</div>
-<div className={s.middle}>Телега сдача-покраска</div>
-<div className={s.middle}>Телега-яма</div>
-<div className={s.middle}>dd</div>
-<div className={s.middle}>dd</div>
+<div className={s.middle}>Телега сдача</div>
+<div className={s.smallMal}>
+  <div className={s.slMal}>Телега-яма</div>
+  
+</div>
+<div className={s.middle}>Дорога</div>
+<div className={s.tel7}><div className={s.middl}>Телега 7-пролет</div></div>
 
 </div>
 
@@ -29,7 +32,7 @@ export default function Gate() {
     <div className={[s.rightgate2,s.gateItem_right].join(' ')}></div>
     <div className={[s.rightgate3,s.gateItem_right].join(' ')}></div>
     <div className={[s.rightgate4,s.gateItem_right].join(' ')}></div>
-    <div className={[s.rightgate5,s.gateItem_right].join(' ')}>5</div>
+    <div className={[s.rightgate5,s.gateItem_right].join(' ')}><span>проезд</span></div>
     <div className={[s.rightgate6,s.gateItem_right].join(' ')}></div>
 </div>
 
