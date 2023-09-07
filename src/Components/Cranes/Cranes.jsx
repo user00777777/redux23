17bd5a28c15d33484d71f52a9b223614cc64756b
user00777777 <misterborman@gmail.com>
2023-09-08 00:03:50 +0300

    dd

--- src/Components/Cranes/Cranes.jsx
+++ src/Components/Cranes/Cranes.jsx
@@ -4,13 +4,31 @@ export default function Cranes() {
   return (
 <div className={s.container}>
     <div className={s.wrapper}>
-    <div className={[s.newEstakada,s.item].join(' ')}>Новая эстакада</div>
+    <div className={[s.newEstakada,s.item].join(' ')}>
+      <div className={s.abs}><div>Малярка</div></div>
+      <div className={[s.mal,s.mal1].join(' ')} ><span>Кран <b>2</b></span>
+       </div>
+<div className={[s.mal,s.mal2].join(' ')} ><span>Кран <b>1</b></span>
+{/* <img src="https://www.svgrepo.com/show/242143/hook-crane.svg" alt="" /> */}
+  </div>
+  </div>
 
-        <div className={[s.district6,s.item].join(' ')}></div>
-        <div className={[s.district7,s.item].join(' ')}></div>
+        <div className={[s.district6,s.item].join(' ')}>
+        <div className={s.abs}><div>6-й Пролет</div></div>
+          
+        <div  className={[s.cran1,s.mal].join(' ')}> Кран<b>№3</b> </div>
+  <div className={[s.cran2,s.mal].join(' ')}>Кран<b>№2</b> </div>
+  <div className={[s.cran3,s.mal].join(' ')}>Кран<b>№1</b> </div>
+
+        </div>
+        <div className={[s.district7,s.item].join(' ')}>
+        <div className={s.abs}><div>7-й Пролет</div></div>
+
+        <div className={[s.cran2,s.mal].join(' ')}>Кран<b>№2</b> </div>
+  <div className={[s.cran3,s.mal].join(' ')}>Кран<b>№1</b> </div>
+        </div>
         
         </div>
-
 
 
 
