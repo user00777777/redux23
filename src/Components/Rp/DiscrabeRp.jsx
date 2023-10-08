ea84ca018af9f59f9ec4d7d46f90d6a9d72c5cd5
user00777777 <misterborman@gmail.com>
2023-10-08 23:27:11 +0300

    table

--- src/Components/Rp/DiscrabeRp.jsx
+++ src/Components/Rp/DiscrabeRp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import s from './css/discrabe.module.css'
 import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
@@ -12,7 +13,7 @@ export default function DiscrabeRp() {
 	const tableRef = useRef()
 	const table = tableRef.current
 	console.log(table)
-	const [el, setElement] = useState()
+	const [el, setElement] = useState({})
 
 	const $table = document.createElement('table'),
 		$tbody = document.createElement('tbody')
@@ -21,38 +22,35 @@ export default function DiscrabeRp() {
 	useEffect(() => {
 		state?.forEach(el => {
 			setElement(el)
-
-			// create_tr(el.name, el.nom)
 		})
-	}, [])
+	}, [state])
 
-	console.log(el.name)
+	console.log(state)
 
 	return (
-		<div>
-			<div>
+		<div className={s.container}>
+			<div className={s.btnWrap}>
 				<button onClick={() => navigate(-1)}>back</button>
 			</div>
-			<div ref={tableRef}>
-				<table>
-					<tbody>
-						<tr>
-							<th>Название</th>
-							<th>Номинальный ток</th>
-							<th>Imax</th>
-						</tr>
-						<tr>
-							<td>11</td>
-							<td>222</td>
-							<td>333</td>
-						</tr>
-						<tr>
-							<td>11</td>
-							<td>222</td>
-							<td>333</td>
-						</tr>
-					</tbody>
-				</table>
+			<ul className={s.wrapName}>
+				<li>Наименование</li>
+				<li>
+					<span>ток</span> уставки
+				</li>
+				<li>ном-й Ток</li>
+				<li>квт</li>
+			</ul>
+			<div ref={tableRef} className={s.wrapper}>
+				{state.map(el => {
+					return (
+						<div className={s.wrapList}>
+							<div className={s.name}> {el.name}</div>
+							<div className={s.i}> {el.I}</div>
+							<div className={s.nom}> {el.nom}</div>
+							<div className={s.kvt}> {el.kvt}</div>
+						</div>
+					)
+				})}
 			</div>
 		</div>
 	)
