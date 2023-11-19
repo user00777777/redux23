b4e32c71c36d7dcf039d83f7c7943537bd965a09
Yurii Petruchenko <misterborman@gmail.com>
2023-11-19 13:03:02 +0200

    de

--- src/Components/machine/Engine.jsx
+++ src/Components/machine/Engine.jsx
@@ -19,23 +19,23 @@ export default function Engine() {
 			{selector.map((li, i) => {
 				return (
 					<ul key={i} className={s.wrapMap}>
-						<li className={`${s.reds} ${s.f}`}>Двигатель</li>
-						<li className={`${s.Motor_starterA1} ${s.f}`}>пускателя</li>
-						<li className={`${s.power} ${s.f}`}>мощность</li>
+						<li className={`${s.engineCurrent} ${s.f}`}>Номинальный</li>
+						<li className={`${s.Motor_starterA1} ${s.f}`}>Мощность</li>
+						<li className={`${s.power} ${s.f}`}>cosF</li>
 						<li className={`${s.rpm} ${s.f}`}>оброты</li>
-						<li className={`${s.protection} ${s.f}`}>Автомат</li>
+						<li className={`${s.cosF} ${s.f}`}>вес двигателя</li>
+						<li className={`${s.shaftDiameter1} ${s.f}`}>диаметр вала</li>
 						{/* <div className={s.second} key={i}> */}
 						{/* <li className={s.power1}> {li.mainEngine.power}</li> */}
 						<li className={s.mainEngine_power}> {li.mainEngine.power}</li>
-						<li className={s.mainEngine_starterA1}>
-							{' '}
-							{li.mainEngine.Motor_starterA1}
-						</li>
+						<li className={s.mainEngine_starterA1}> {li.mainEngine.cosF}</li>
 						<li className={s.engineName}> {li.mainEngine.engineName}</li>
 						<li className={s.mainEngine_rpm}> {li.mainEngine.Engine_rpm}</li>
-						<li className={s.CircuitBreaker}>
+						<li className={s.CircuitBreaker}> {li.mainEngine.weight}</li>
+						{/* <li className={s.shaftDiameter}> {li.mainEngine.shaftDiameter}</li> */}
+						<li className={s.electricCurrent}>
 							{' '}
-							{li.mainEngine.CircuitBreaker}
+							{li.mainEngine.electricCurrent}
 						</li>
 						{/* </div> */}
 					</ul>
--- src/sliceReduce/engineREduser.js
+++ src/sliceReduce/engineREduser.js
@@ -7,8 +7,12 @@ const initialState = {
 				Engine_rpm: '975 Об',
 				power: '18,5kw',
 				Motor_starterA1: 'A1:KM4,A2:KM5',
-
+				cosF: 0.87,
 				CircuitBreaker: 'A1:QF13,A2:QF14',
+				motorShaft: '180mm',
+				weight: '160кг',
+				electricCurrent: '36A',
+				shaftDiameter: '55mm',
 			},
 		},
 	],
