4b94c981c71143deeb87ef694f0c38d87e77ce27
Yurii Petruchenko <misterborman@gmail.com>
2023-12-03 20:24:49 +0200

    plumb

--- src/css/homeComponent/Plambers/PlumbersDuty.jsx
+++ src/css/homeComponent/Plambers/PlumbersDuty.jsx
@@ -10,44 +10,60 @@ function plumb(pis, babec, golovch, nemch, hours, day) {
 	// console.log(oneShift[day % oneShift.length])
 	// console.log(twoShift)
 	// console.log(hours >= 7 && hours < 19)
-	console.log(hours)
-
+	// console.log(hours=8)
+	day += 2
 	if (hours >= 7 && hours < 19) {
-		console.log(1)
+		// console.log(1)
 
 		let plumber = oneShift[day % oneShift.length]
-		console.log(plumber)
+		// console.log(plumber)
 
 		return plumber
 	}
 
 	if (hours >= 19 && hours <= 23) {
-		console.log('oo')
+		// console.log('oo')
 
 		let plumber2 = twoShift[day % twoShift.length]
-		console.log(plumber2, day)
+		// console.log(plumber2, day)
 
 		return plumber2
 	} else {
 		let plumber2 = twoShift[--day % twoShift.length]
 		console.log(plumber2)
 
-		console.log(day)
-
+		// console.log(day)
+		// day += 1
 		return plumber2
 	}
 }
 //
 
 export default function PlumbersDuty() {
-	const [plumber1, setPlumber] = useState('')
+	const [newDay, setDay] = useState()
+	let [new_Day, set_Day] = useState(1)
 	let date = new Date(),
 		day = date.getDate(),
 		hours = date.getHours(),
 		minutes = date.getMinutes()
+	let arrDay = [1, 2, 3, 4]
 
+	const ChangeDay = () => {
+		set_Day(() => (new_Day += 1))
+		let x = arrDay[new_Day % arrDay.length]
+		// console.log(x)
+
+		localStorage.setItem('plumber', x)
+
+		let strNum = Number(localStorage.getItem('plumber'))
+		// console.log(strNum)
+
+		setDay(strNum)
+	}
+	// console.log(typeof newDay)
+	day += newDay
 	const oneShift = plumb(
-		'Писареенко',
+		'Дубовык',
 		'Бабец',
 		'Головченко',
 
@@ -55,11 +71,11 @@ export default function PlumbersDuty() {
 		hours,
 		day
 	)
-	console.log(oneShift)
+	// console.log(oneShift)
 
 	return (
 		<div className={s.container}>
-			<div className={s.plumbers}>
+			<div className={s.plumbers} onClick={ChangeDay}>
 				Дежурный сантехник: <h2>{oneShift}</h2>
 			</div>
 		</div>
