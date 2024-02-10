4ec9344363c66157c550782c5e36ca1a86644e86
Yurii Petruchenko <misterborman@gmail.com>
2024-02-10 22:46:26 +0200

    1/6

--- src/sliceReduce/fotoCranes/foto1_6.js
+++ src/sliceReduce/fotoCranes/foto1_6.js
@@ -1,10 +1,16 @@
-import crane6_1 from '../../Components/Cranes/FotoCrane/1CD6.jpg'
-import crane6_1_2 from '../../Components/Cranes/FotoCrane/1Cd6_.jpg'
-import crane6_1_3 from '../../Components/Cranes/FotoCrane/1Crane6District.jpg'
-import crane6_1_4 from '../../Components/Cranes/FotoCrane/1CraneD6.jpg'
+// import crane6_1 from '../../Components/Cranes/FotoCrane/one_Crane/1_6.jpg'
+// import crane6_1_2 from '../../Components/Cranes/FotoCrane/1Cd6_.jpg'
+// import crane6_1_3 from '../../Components/Cranes/FotoCrane/1Crane6District.jpg'
+// import crane6_1_4 from '../../Components/Cranes/FotoCrane/1CraneD6.jpg'
+import crane1_6Lift from '../../Components/Cranes/FotoCrane/one_Crane/1_6Lift.jpg'
+import crane1_6full from '../../Components/Cranes/FotoCrane/one_Crane/full1_6.jpg'
+import crane1_6_manage from '../../Components/Cranes/FotoCrane/one_Crane/sManage1_6.jpg'
+import crane1_6_trip from '../../Components/Cranes/FotoCrane/one_Crane/tripEngine1_6.jpg'
+import crane1_6Half from '../../Components/Cranes/FotoCrane/one_Crane/1_6.jpg'
+import crane1_6HalLift from '../../Components/Cranes/FotoCrane/one_Crane/1_6m.jpg'
 
 function crane1_6 () {
-return [crane6_1,crane6_1_2,crane6_1_3,crane6_1_4]
+return [ crane1_6full, crane1_6_manage, crane1_6_trip, crane1_6Lift,crane1_6Half,crane1_6HalLift]
 
 }
 export default crane1_6
--- src/sliceReduce/fotoCranes/foto2_6.js
+++ src/sliceReduce/fotoCranes/foto2_6.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import cond from '../../Components/Cranes/FotoCrane/two_Crene/2_6_cond.jpg'
 
-import full_2_1 from '../../Components/Cranes/FotoCrane/two_Crene/full1_6.jpg'
+import full_2_1 from '../../Components/Cranes/FotoCrane/two_Crene/2_6_full.jpg'
 
-import lift from '../../Components/Cranes/FotoCrane/two_Crene/lift.jpg'
+import liftManage from '../../Components/Cranes/FotoCrane/two_Crene/2_6LiftM_.jpg'
+import liftMan from '../../Components/Cranes/FotoCrane/two_Crene/2_6LiftManage.jpg'
 import tripEngine from '../../Components/Cranes/FotoCrane/two_Crene/tripEngine1_6.jpg'
-import tripManage from '../../Components/Cranes/FotoCrane/two_Crene/tripManage1_6.jpg'
-import sManage from '../../Components/Cranes/FotoCrane/two_Crene/sManage1_6.jpg'
+import tripManage from '../../Components/Cranes/FotoCrane/two_Crene/2_6tripMenege.jpg'
+import sManage from '../../Components/Cranes/FotoCrane/two_Crene/2_6Lin.jpg'
+import sManage2 from '../../Components/Cranes/FotoCrane/two_Crene/2_6LinM.jpg'
 export default function foto2_6() {
-	return [ full_2_1,lift,tripEngine,tripManage,sManage]
+	return [ full_2_1,liftMan,liftManage,tripEngine,tripManage,sManage]
 }
