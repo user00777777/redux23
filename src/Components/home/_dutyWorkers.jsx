5578da6bc2d2b161a4d8d47046fe07ac50a88f7d
yurii petrychenko <misterborman@gmail.com>
2024-08-12 22:45:12 +0300

    duty

--- src/Components/home/_dutyWorkers.jsx
+++ src/Components/home/_dutyWorkers.jsx
@@ -8,57 +8,60 @@ export default function getDuty(electr, plumb, gas) {
 
 	const currentDate = new Date()
 	let dayOfMonth = currentDate.getDate()
-	console.log(dayOfMonth);
+	// console.log(dayOfMonth);
 
 	// const dayOfWeek = currentDate.getDay()
 	let hour = currentDate.getHours()
 	const minutes = currentDate.getMinutes()
-	const month = currentDate.getMonth()+1
+	const month = currentDate.getMonth() + 1
 
 	let dayOfYear = Math.floor(
 		(currentDate - new Date(currentDate.getFullYear(), 0, 0)) / 86400000
 	)
-  // console.log(month);
-  
-	// console.log((dayOfYear ))
-  // console.log(dayOfMonth=31);
-  // console.log(hour=24);
-  
-  
+	// console.log(month);
+
+	// console.log((dayOfYear )+3)
+	// console.log((dayOfMonth = 30))
+	// console.log(hour=24);
 
 	// hour = 1
 
 	let duty = (one, two, three, four) => {
-
-
-
-
 		let firstOneShift = [one, two, three, four]
 
+		let oneShift = firstOneShift.map(el => {
+			if (
+				el.toLowerCase() == 'круглый' &&
+				dayOfMonth >= 12 &&
+				dayOfMonth <= 23 &&
+				month == 8
+			) {
+				// console.log('ok')
 
-let oneShift = firstOneShift.map(el => {
+				return (el = 'Гекало')
+			}
 
-  
+			if (
+				el.toLowerCase() == 'немченко' &&
+				dayOfMonth >= 17 &&
+				dayOfMonth <= 31 &&
+				month == 8
+			) {
+				el = 'Павлюченко';
+        return el
+			} else {
+				// console.log('no');
 
-	if (el.toLowerCase() == 'круглый' && dayOfMonth >= 9 && dayOfMonth <= 31&&month==4) {
-		console.log('ok')
-
-		return (el = 'Гекало')
-	} else {
-    console.log('no');
-    
-		return el
-	}
-})
-
-
+				return el
+			}
+		})
 
 		const twoShift = [...oneShift.slice(3), ...oneShift.slice(0, 3)]
 		// console.log('two', twoShift)
-		// console.log(dayOfMonth)
+		// console.log(oneShift,'one')
 
 		if (hour >= 7 && hour < 19) {
-			console.log('day')
+			// console.log('day')
 
 			return { shift: oneShift, bool: true }
 		}
@@ -68,9 +71,8 @@ let oneShift = firstOneShift.map(el => {
 			return { shift: twoShift, bool: true }
 		} else {
 			console.log('night')
-      let twoChangeShift = [...oneShift.slice(2), ...oneShift.slice(0, 2)]
-      // console.log(twoChangeShift);
-      
+			let twoChangeShift = [...oneShift.slice(2), ...oneShift.slice(0, 2)]
+			console.log(twoChangeShift)
 
 			return { shift: twoChangeShift, bool: false }
 		}
@@ -79,16 +81,19 @@ let oneShift = firstOneShift.map(el => {
 	let dutyGas = duty(...gas.gas)
 	let dutyPlumb = duty(...plumb.plumb)
 	let dutyEl = duty(...electr.el)
+  console.log(dutyPlumb);
+  
 	const handleDuty = (el, elBool = true) => {
-    // console.log(el);
-    
-		// console.log(elBool)
-// dayOfYear=dayOfYear-1
+		// console.log(el);
+// console.log(dayOfYear += 3)
 
+		// console.log(elBool)
+		// dayOfYear=dayOfYear-1
+console.log([dayOfYear % el.length])
 
 		let duty = el[dayOfYear % el.length]
 
-		// console.log(duty)
+		console.log(duty)
 
 		return duty
 	}
@@ -96,7 +101,7 @@ let oneShift = firstOneShift.map(el => {
 	const manGas = handleDuty(dutyGas.shift, dutyGas.bool)
 	const manPlumb = handleDuty(dutyPlumb.shift, dutyPlumb.bool)
 	const manEl = handleDuty(dutyEl.shift, dutyEl.bool)
-	console.log(manEl, manGas, manPlumb)
+	// console.log(manEl, manGas, manPlumb)
 
 	return (
 		<ul>
