900e3db2ea3fb429167c61dc9de6c045c5178e5e
yurii petrychenko <misterborman@gmail.com>
2024-08-18 23:14:46 +0300

    regscss

--- src/Components/Cranes/RegCrane.jsx
+++ src/Components/Cranes/RegCrane.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import s from './reg.module.css'
 import { useSelector } from 'react-redux'
-export default function RegCrane({onClick}) {
+export default function RegCrane({ onClick }) {
 	const cranes = useSelector(state => state.cranes.craneObj)
 	// console.log(cranes)
 
-	let { Q = '', craneId,inv } = cranes || {}
+	let { Q = '', craneId, inv } = cranes || {}
 	// console.log(Q) // ''
-	// console.log(craneId) // 
+	// console.log(craneId) //
 
-  // console.log(inv,'inv')
+	// console.log(inv,'inv')
 
 	return (
 		<div onClick={onClick} className={s.container}>
 			<ul className={s.wrapper}>
-				<li className={`${s.item} ${s.item1}`}>Q={Q}</li>{' '}
-				<li className={`${s.item} ${s.item2}`}>инв№{inv}</li>{' '}
-				<li className={`${s.item} ${s.item3}`}>рег№{craneId}</li>{' '}
+				<li className={`${s.item} ${s.item1}`}> Q={Q} </li>{' '}
+				<li className={`${s.item} ${s.item2}`}> инв№ {inv} </li>{' '}
+				<li className={`${s.item} ${s.item3}`}> рег№{craneId} </li>{' '}
 			</ul>
 		</div>
 	)
