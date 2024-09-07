d1a0b2dbe2f3c90667e442e2b2fe6e32ca23b7f9
yurii petrychenko <misterborman@gmail.com>
2024-09-07 21:41:55 +0300

    inspect all

--- src/Components/Inspection/Inspection.jsx
+++ src/Components/Inspection/Inspection.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './inspection.module.css'
 import BackPage from '../BackPage/BackPage'
 import OneTelfInspect from './OneTelfInspect'
 import { useSelector } from 'react-redux'
 
 export default function Inspection() {
+	const [strRes, setStrRes] = useState([])
+	let handleClick = str => {
+		switch (str) {
+			case 'десять':
+			case 'месяц':
+			case 'полгода':
+			case 'год':
+			case 'два года':
+				setStrRes(prevStrRes => {
+					if (!prevStrRes.includes(str)) {
+						return [...prevStrRes, str]
+					}
+					return prevStrRes
+				})
+				break
+			default:
+				break
+		}
 
+		// console.log(str)
+	}
+	console.log(strRes)
 
 	return (
 		<div className={s.containerInspect} id={s.c}>
@@ -15,11 +36,41 @@ export default function Inspection() {
 				<h2>Тех-осмотр тельфера {'ОЦЛП'}</h2>
 			</div>
 			<div className={s.wrapper}>
-				<OneTelfInspect daysDifference={{ num: 10, strDays: 'десять' }}     / >
-				<OneTelfInspect daysDifference={{ num: 30, strDays: 'месяц' }} />
-				<OneTelfInspect daysDifference={{ num: 61, strDays: 'полгода' }} />
-				<OneTelfInspect daysDifference={{ num: 365, strDays: 'год' }} />
-				<OneTelfInspect daysDifference={{ num: 740, strDays: 'два года' }} />
+		{	strRes.includes('десять') &&	<OneTelfInspect
+					daysDifference={{
+						num: 10,
+						strDays: 'десять',
+						handleClick: handleClick,
+					}}
+				/>}
+		{	strRes.includes('месяц') &&				<OneTelfInspect
+					daysDifference={{
+						num: 30,
+						strDays: 'месяц',
+						handleClick: handleClick,
+					}}
+				/>}
+	{	strRes.includes('полгода') &&				<OneTelfInspect
+					daysDifference={{
+						num: 61,
+						strDays: 'полгода',
+						handleClick: handleClick,
+					}}
+				/>}
+	{	strRes.includes('год') &&		<OneTelfInspect
+					daysDifference={{
+						num: 365,
+						strDays: 'год',
+						handleClick: handleClick,
+					}}
+				/>}
+			{strRes.includes('два года') &&	<OneTelfInspect
+					daysDifference={{
+						num: 740,
+						strDays: 'два года',
+						handleClick: handleClick,
+					}}
+				/>}
 			</div>
 		</div>
 	)
--- src/Components/Inspection/OneTelfInspect.jsx
+++ src/Components/Inspection/OneTelfInspect.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './inspection.module.css'
 import { useSelector } from 'react-redux'
 import { Accordion, AccordionItem } from '@szhsin/react-accordion'
@@ -6,17 +6,66 @@ import { Accordion, AccordionItem } from '@szhsin/react-accordion'
 export default function OneTelfInspect({ daysDifference }) {
 	const state = useSelector(state => state.ispectHoist)
 	let newState = [...state]
+	const [ind, SetIn] = useState(true)
 
-	console.log(daysDifference?.strDays)
+	// console.log(daysDifference?.strDays)
 
-	// Сортировка по разнице в днях
 	let sortedState = newState.sort((a, b) => a.daysDifference - b.daysDifference)
 
-	// sortedState
-	// 	.filter(el => Number(el.daysDifference) < 1 && el.daysDifference >= 0)
-	// 	.map(el => {
-	// 		console.log(el.telf)
-	// 	})
+	if (daysDifference?.handleClick) {
+		// daysDifference?.handleClick(daysDifference?.strDays, daysDifference?.num)
+		newState.filter(el => {
+      // console.log(Number(el.daysDifference))
+      
+			let year =
+				Number(el.daysDifference) < daysDifference.num &&
+				el.daysDifference >= 0 &&
+				daysDifference?.strDays === 'год'
+			let month =
+				Number(el.daysDifference) < daysDifference.num &&
+				el.daysDifference >= 0 &&
+				daysDifference?.strDays === 'месяц'
+			let  quarter =
+				Number(el.daysDifference) < daysDifference.num &&
+				el.daysDifference >= 0 &&
+				daysDifference?.strDays === 'полгода'
+			let ten =
+				Number(el.daysDifference) < daysDifference.num &&
+				el.daysDifference >= 0 &&
+				daysDifference?.strDays === 'десять'
+			let twoYears =
+				Number(el.daysDifference) < daysDifference.num &&
+				el.daysDifference >= 0 &&
+				daysDifference?.strDays === 'два года'
+      // console.log(r);
+      
+      if (year) {
+				// console.log(daysDifference?.strDays)
+
+				daysDifference?.handleClick(daysDifference?.strDays)
+			}
+      if (month) {
+				// console.log(daysDifference?.strDays)
+
+				daysDifference?.handleClick(daysDifference?.strDays)
+			}
+      if (quarter) {
+				// console.log(daysDifference?.strDays)
+
+				daysDifference?.handleClick(daysDifference?.strDays)
+			}
+      if (ten) {
+				// console.log(daysDifference?.strDays)
+
+				daysDifference?.handleClick(daysDifference?.strDays)
+			}
+      if (twoYears) {
+				// console.log(daysDifference?.strDays)
+
+				daysDifference?.handleClick(daysDifference?.strDays)
+			}
+		})
+	}
 
 	return (
 		<Accordion className={s.headAccord}>
@@ -41,18 +90,20 @@ export default function OneTelfInspect({ daysDifference }) {
 								Number(el.daysDifference) < daysDifference.num &&
 								el.daysDifference >= 0
 						)
-						.map((el, id) => (
-							<div key={id}>
-								<ul className={s.wrapContent} id={s.wrapTelf}>
-									<li className={`${s.telf} ${s.telfer}`}> {el.telf}</li>
-									<li className={`${s.telf} ${s.check1}`}>{el.start}</li>
-									<li className={`${s.telf} ${s.check1}`}>{el.end}</li>
-									<li className={`${s.telf}`}>{el.q}</li>
-									<li className={`${s.telf}`}>{el.area}</li>
-									<li className={`${s.telf}`}>{el.exactLocation}</li>
-								</ul>
-							</div>
-						))}
+						.map((el, id) => {
+							return (
+								<div key={id}>
+									<ul className={s.wrapContent} id={s.wrapTelf}>
+										<li className={`${s.telf} ${s.telfer}`}> {el.telf}</li>
+										<li className={`${s.telf} ${s.check1}`}>{el.start}</li>
+										<li className={`${s.telf} ${s.check1}`}>{el.end}</li>
+										<li className={`${s.telf}`}>{el.q}</li>
+										<li className={`${s.telf}`}>{el.area}</li>
+										<li className={`${s.telf}`}>{el.exactLocation}</li>
+									</ul>
+								</div>
+							)
+						})}
 				</div>
 			</AccordionItem>
 		</Accordion>
