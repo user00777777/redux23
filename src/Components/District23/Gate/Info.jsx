66470aa92f23a0f6326d0df28ad10c71146020ba
yurii petrychenko <misterborman@gmail.com>
2024-10-12 10:26:08 +0300

    redoGate

--- src/Components/District23/Gate/Info.jsx
+++ src/Components/District23/Gate/Info.jsx
@@ -1,41 +1,41 @@
 import React, { useRef } from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import CircuitGate from '../CircuitGate'
 import s from './cssGate/oneGate.module.css'
 
 export default function Info({ my }) {
 	const [bool, setBool] = useState(false)
 	let wrRef = useRef()
 
-	// console.log(my)
-
 	function f() {
 		if (my.gate == 11 || my.gate == 3) return 'Схема'
 		return false
 	}
 
+	const gateScheme = f()
+
 	return (
 		<div className={s.con}>
 			<ul ref={wrRef} className={s.wrap}>
-				<li className={[s.item, s.item1, s.blinck]}>
+				<li className={[s.item, s.item1, s.blinck].join(' ')}>
 					Ворота <b>№{my?.gate}</b>
 				</li>
-				<li className={[s.item, s.item2]}>
+				<li className={[s.item, s.item2].join(' ')}>
 					Запитано от
 					<br />
 					<b> {my.rp}</b>
 				</li>
-				<li className={[s.item, s.item3]}>
+				<li className={[s.item, s.item3].join(' ')}>
 					Автомат <b>{my.avt}</b>
 				</li>
-				<li className={[s.item, s.item4]}>
-					Двигатель: <b>{my.engine} kw</b>
-				</li>{' '}
-				{f() && (
-					<li className={[s.item, s.item5]}>
-						{' '}
-						<Link to='/circuitGate'> {f()}</Link>
+				{my.engine && (
+					<li className={[s.item, s.item4].join(' ')}>
+						Двигатель: <b>{my.engine} kw</b>
+					</li>
+				)}
+				{gateScheme && (
+					<li className={[s.item, s.item5].join(' ')}>
+						<Link to='/circuitGate'> {gateScheme}</Link>
 					</li>
 				)}
 			</ul>
